f4cc73c24982258f926771cd0070c19d4e6bd75a
Pierre Dunn <vologdin.ia@amntksoyuz.ru>
2026-05-05 12:28:22 +0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM python:3.12-alpine
 WORKDIR /app
+COPY requirements.txt .
+RUN pip install --upgrade pip && pip install -r requirements.txt
 COPY . .
-RUN pip install --upgrade pip
-RUN pip install -r requirements.txt
 RUN python manage.py collectstatic --noinput
-RUN pip install gunicorn
 EXPOSE 8000
 CMD ["sh", "-c", "python manage.py migrate && gunicorn sitenigger.wsgi:application --bind 0.0.0.0:8000"]
--- sitenigger/settings.py
+++ sitenigger/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b0w^tu&7vbdg67#75qtjgkl#m39a=2$78vlghd=@vsvpd-dgr6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 INTERNAL_IPS = [
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -184,3 +185,4 @@ CKEDITOR_5_CONFIGS = {
         }
     }
 }
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
