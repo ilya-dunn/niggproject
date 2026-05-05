67782b4492aba5a47aaf884dbb379805b52c7890
Pierre Dunn <vologdin.ia@amntksoyuz.ru>
2026-05-05 13:13:43 +0300

    fix

--- sitenigger/settings.py
+++ sitenigger/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b0w^tu&7vbdg67#75qtjgkl#m39a=2$78vlghd=@vsvpd-dgr6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 INTERNAL_IPS = [
@@ -186,3 +186,7 @@ CKEDITOR_5_CONFIGS = {
     }
 }
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+]
