05067995feca5e4756c6922f1d721cc6bb4b38fa
Pierre Dunn <vologdin.ia@amntksoyuz.ru>
2026-05-05 15:18:20 +0300

    fix

--- sitenigger/settings.py
+++ sitenigger/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b0w^tu&7vbdg67#75qtjgkl#m39a=2$78vlghd=@vsvpd-dgr6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "niggproject-production.up.railway.app",
@@ -211,3 +211,9 @@ LOGGING = {
         },
     },
 }
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
